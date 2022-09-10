2c1473f36c3f3b9cd20bb07f8ee19e24f33fc571
maurojjzz <maurojim123@gmail.com>
2022-09-10 14:46:06 -0300

    ejericio 1.2

---
+++ dist/1.2.js
@@ -0,0 +1,14 @@
+"use strict";
+/*Dado el siguiente codigo:
+Inspeccionar el tipo de ‘torta‘
+Añadir una anotación de tipo explícito a ‘torta‘
+Intenta asignar tipos inválidos, por diversión*/
+
+//Ej Con tipo invvalido en linea 9 no se puede asignar un string a un booleano
+// let torta:boolean;
+// torta = 'arandanos';
+// console.log('[Ejercicio 1.2]', `Me gusta comer torta con sabor a ${torta}.`);
+
+let torta;
+torta = 'arandanos';
+console.log('[Ejercicio 1.2]', `Me gusta comer torta con sabor a ${torta}.`);
---
+++ src/1.2.ts
@@ -0,0 +1,14 @@
+/*Dado el siguiente codigo:
+Inspeccionar el tipo de ‘torta‘
+Añadir una anotación de tipo explícito a ‘torta‘
+Intenta asignar tipos inválidos, por diversión*/
+
+
+//Ej Con tipo invvalido en linea 9 no se puede asignar un string a un booleano
+// let torta:boolean;
+// torta = 'arandanos';
+// console.log('[Ejercicio 1.2]', `Me gusta comer torta con sabor a ${torta}.`);
+
+let torta:string;
+torta = 'arandanos';
+console.log('[Ejercicio 1.2]', `Me gusta comer torta con sabor a ${torta}.`);
