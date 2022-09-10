8ab66a9f5b9dd310469cbf4f8a0f4c6f9ceb901e
maurojjzz <maurojim123@gmail.com>
2022-09-10 13:38:00 -0300

    first commit

---
+++ dist/1.1.js
@@ -0,0 +1,4 @@
+"use strict";
+let pi = '3.14159';
+let tau = parseFloat(pi) * 2;
+console.log('[Ejercicio 1.1]', `${tau} es ${pi} veces el dos.`);
---
+++ src/1.1.ts
@@ -0,0 +1,4 @@
+let pi = '3.14159';
+let tau = parseFloat(pi) * 2;
+
+console.log('[Ejercicio 1.1]', `${tau} es ${pi} veces el dos.`);
