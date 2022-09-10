3b743e74f685bf8665971462f2bf8b881da8d88b
maurojjzz <maurojim123@gmail.com>
2022-09-10 13:44:40 -0300

    otra resolucion ejercicio 1.1

--- dist/1.1.js
+++ dist/1.1.js
@@ -1,4 +1,8 @@
 "use strict";
-let pi = '3.14159';
-let tau = parseFloat(pi) * 2;
+// let pi = '3.14159';
+// let tau = parseFloat(pi) * 2;
+// console.log('[Ejercicio 1.1]', `${tau} es ${pi} veces el dos.`);
+
+let pi = 3.14159;
+let tau = pi * 2;
 console.log('[Ejercicio 1.1]', `${tau} es ${pi} veces el dos.`);
--- src/1.1.ts
+++ src/1.1.ts
@@ -1,4 +1,7 @@
-let pi = '3.14159';
-let tau = parseFloat(pi) * 2;
+// let pi = '3.14159';
+// let tau = parseFloat(pi) * 2;
+// console.log('[Ejercicio 1.1]', `${tau} es ${pi} veces el dos.`);
 
+let pi = 3.14159;
+let tau = pi * 2;
 console.log('[Ejercicio 1.1]', `${tau} es ${pi} veces el dos.`);
