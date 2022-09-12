7d5b66baa754f6b4998cb77b06f3f069fac77ab2
maurojjzz <maurojim123@gmail.com>
2022-09-12 13:10:42 -0300

    4.*

---
+++ dist/4.0.js
@@ -0,0 +1,18 @@
+"use strict";
+//TypeScript es inteligente sobre los posibles tipos de una variable, dependiendo de la ruta del código.
+function doStuff(value) {
+    if (typeof value === 'string') {
+        console.log(value.toUpperCase().split('').join(' '));
+    }
+    else if (typeof value === 'number') {
+        console.log(value.toPrecision(5));
+    }
+    value; // coloque el cursor sobre `valor` aqui
+}
+doStuff(2);
+doStuff(22);
+doStuff(222);
+doStuff('hello');
+doStuff(true);
+doStuff({});
+console.log('[Ejercicio 4.1]');
