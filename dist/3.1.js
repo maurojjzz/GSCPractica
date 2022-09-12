9df5ae9e5f41a5e48d702d637df5afa37717c5e6
maurojjzz <maurojim123@gmail.com>
2022-09-12 01:15:01 -0300

    3.2

--- dist/3.1.js
+++ dist/3.1.js
@@ -3,7 +3,7 @@ function add(x, y) {
     return x + y;
 }
 function sumArray(numbers) {
-    return numbers.reduce(add, 8);
+    return numbers.reduce(add, 0);
 }
 const someSum = sumArray([3, 6, 9]);
 console.log('[Ejercicio 3.1]', `3 + 6 + 9 === ${someSum}`);
