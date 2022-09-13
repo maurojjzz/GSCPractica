dd63df53a63396d2e9bf2670568f05d35d251886
maurojjzz <maurojim123@gmail.com>
2022-09-13 01:28:48 -0300

    4.*

--- dist/4.1.js
+++ dist/4.1.js
@@ -1,28 +1,45 @@
 "use strict";
+//comentado porque en 4.4 hay clases e interfaces que se llaman igual
+/*
+interface EggLayer {
+    layEggs(): void;
+}
+
+interface Flyer {
+    fly(height: number): void;
+}
+
+interface Swimmer {
+    swim(depth: number): void;
+}
+
+
 // agregar alias de tipo(s) aqui
-class Bird {
-    constructor(species) {
-        this.species = species;
+  
+class Bird implements EggLayer,Flyer {
+    constructor(public species: string) { }
+       
+    layEggs(): void {
+    console.log('Poniendo huevos de aves.');
     }
-    layEggs() {
-        console.log('Poniendo huevos de aves.');
+       
+    fly(height: number): void {
+    console.log(`Volando a la altura de ${height} metros.`);
     }
-    fly(height) {
-        console.log(`Volando a la altura de ${height} metros.`);
+};
+
+class Fish implements EggLayer,Swimmer {
+    constructor(public species: string) { }
+       
+    layEggs(): void {
+    console.log('Poniendo huevos de pescado.');
+    }
+       
+    swim(depth: number): void {
+    console.log(`Nadando a una profundidad de ${depth} metros.`);
     }
 }
-;
-class Fish {
-    constructor(species) {
-        this.species = species;
-    }
-    layEggs() {
-        console.log('Poniendo huevos de pescado.');
-    }
-    swim(depth) {
-        console.log(`Nadando a una profundidad de ${depth} metros.`);
-    }
-}
+      
 function getRandomAnimal() {
     const animals = [
         new Bird('puffin'),
@@ -30,14 +47,17 @@ function getRandomAnimal() {
         new Fish('sea robin'),
         new Fish('leafy seadragon'),
     ];
+        
     return animals[Math.floor(Math.random() * animals.length)];
 }
+       
 function interrogateAnimal(animal = getRandomAnimal()) {
     animal.layEggs();
-    if (animal instanceof Fish)
-        animal.swim(10); // se llama solo si es un pez
-    else if (animal instanceof Fish)
-        animal.fly(10); // se llama solo si es un pajaro
+    if(animal instanceof Fish) animal.swim(10) // se llama solo si es un pez
+    else if(animal instanceof Fish) animal.fly(10); // se llama solo si es un pajaro
+       
     return animal.species;
-}
-console.log('[Ejercicio 4.4]', `Tenemos un ${interrogateAnimal()} en nuestras manos!`);
+    }
+        
+console.log('[Ejercicio 4.4]',`Tenemos un ${interrogateAnimal()} en nuestras manos!`);
+*/ 
--- src/4.4.ts
+++ src/4.4.ts
@@ -1,62 +1,64 @@
-//comentado porque en 4.4 hay clases e interfaces que se llaman igual
-/*
 interface EggLayer {
     layEggs(): void;
 }
-
+    
 interface Flyer {
     fly(height: number): void;
 }
-
+    
 interface Swimmer {
     swim(depth: number): void;
 }
+   
+interface BirdLike extends EggLayer,Flyer{}
+interface FishLike extends EggLayer,Swimmer{}
+
 
 
 // agregar alias de tipo(s) aqui
-  
-class Bird implements EggLayer,Flyer {
+type Animal = Bird | Fish;    
+
+
+class Bird implements BirdLike {
     constructor(public species: string) { }
-       
+
     layEggs(): void {
     console.log('Poniendo huevos de aves.');
     }
-       
+    
     fly(height: number): void {
-    console.log(`Volando a la altura de ${height} metros.`);
+        console.log(`Volando a la altura de ${height} metros.`);
     }
 };
-
-class Fish implements EggLayer,Swimmer {
+    
+class Fish implements FishLike {
     constructor(public species: string) { }
-       
+    
     layEggs(): void {
     console.log('Poniendo huevos de pescado.');
     }
-       
+    
     swim(depth: number): void {
-    console.log(`Nadando a una profundidad de ${depth} metros.`);
+        console.log(`Nadando a una profundidad de ${depth} metros.`);
     }
 }
-      
-function getRandomAnimal() {
+    
+function getRandomAnimal():Animal {
     const animals = [
         new Bird('puffin'),
         new Bird('kittiwake'),
         new Fish('sea robin'),
         new Fish('leafy seadragon'),
     ];
-        
+    
     return animals[Math.floor(Math.random() * animals.length)];
 }
-       
-function interrogateAnimal(animal = getRandomAnimal()) {
-    animal.layEggs();
+    
+function interrogateAnimal(animal:Animal = getRandomAnimal()):string {
     if(animal instanceof Fish) animal.swim(10) // se llama solo si es un pez
-    else if(animal instanceof Fish) animal.fly(10); // se llama solo si es un pajaro
-       
+    else animal.fly(10); // se llama solo si es un pajaro
+    
     return animal.species;
-    }
-        
+}
+
 console.log('[Ejercicio 4.4]',`Tenemos un ${interrogateAnimal()} en nuestras manos!`);
-*/
